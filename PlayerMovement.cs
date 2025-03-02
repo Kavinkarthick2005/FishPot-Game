23b863eef6fa4d6a02137d0ec36da91d3dae24e6
Kavin Karthick R <144412212+Kavinkarthick2005@users.noreply.github.com>
2025-03-02 14:11:03 +0530

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -4,86 +4,101 @@ using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
-//fps movement variable
-public Transform playerCamera;
-[SerializeField][Range(0.0f, 0.5f)] float mouseSmoothTime = 0.03f;
-public bool cursorLock = true;
-public float mouseSensitivity = 3.5f;
-public float Speed= 6.0f;
-[SerializeField][Range(0.0f,0.5f)] float moveSmoothTime = 0.3f;
-public float gravity = -30f;
-public Transform groundCheck;
-public LayerMask ground;
+    // FPS movement variables
+    public Transform playerCamera;
+    [SerializeField] [Range(0.0f, 0.5f)] float mouseSmoothTime = 0.03f;
+    public bool cursorLock = true;
+    public float mouseSensitivity = 3.5f;
+    public float walkSpeed = 6.0f;
+    public float sprintMultiplier = 1.5f; // Sprint multiplier instead of modifying speed
+    [SerializeField] [Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public LayerMask ground;
 
-// height variables
-public float jumpHeight = 6f;
-float velocityY;
-bool isGrounded;
+    // Jumping variables
+    public float jumpHeight = 2f;
+    float velocityY;
+    bool isGrounded;
 
-// mouse set
-float cameraCap;
-Vector2 currentMouseDelta;
-Vector2 currentMouseDeltaVelocity;
+    public Animator anim;
 
-CharacterController controller;
-Vector2 currentDir;
-Vector2 currentDirVelocity;
-Vector3 velocity;
+    // Mouse settings
+    float cameraCap;
+    Vector2 currentMouseDelta;
+    Vector2 currentMouseDeltaVelocity;
 
-    // Start is called before the first frame update
+    CharacterController controller;
+    Vector2 currentDir;
+    Vector2 currentDirVelocity;
+    Vector3 velocity;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
-
-    if (cursorLock)
+        anim = GetComponent<Animator>();
+        if (cursorLock)
         {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
     }
-    // Update is called once per frame
+
     void Update()
     {
-        UpdateMouse();
-        UpdateMove();
-
+        UpdateMouseLook();
+        UpdateMovement();
     }
-    void UpdateMouse(){
-        //mouse look movement
+
+    void UpdateMouseLook()
+    {
+        // Get mouse input
         Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
+        // Smooth mouse movement
         currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
 
+        // Vertical rotation
         cameraCap -= currentMouseDelta.y * mouseSensitivity;
-
         cameraCap = Mathf.Clamp(cameraCap, -90.0f, 90.0f);
-
         playerCamera.localEulerAngles = Vector3.right * cameraCap;
-
+        // Horizontal rotation
         transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
     }
-    void UpdateMove(){
-        //player movement 
-        isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, ground);
 
-        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        targetDir.Normalize();
+    void UpdateMovement()
+{
+    // Check if player is on the ground
+    isGrounded = Physics.CheckSphere(groundCheck.position, 0.3f, ground);
+    if (isGrounded && velocityY < 0) velocityY = -2f;
 
-        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
+    // Get movement input
+    float moveX = Input.GetAxisRaw("Horizontal");
+    float moveZ = Input.GetAxisRaw("Vertical");
+    Vector2 targetDir = new Vector2(moveX, moveZ).normalized;
 
-        velocityY += gravity * 2f * Time.deltaTime;
+    // Check if the player is moving
+    bool isMoving = targetDir.magnitude > 0;
+    bool isRunning = Input.GetKey(KeyCode.LeftShift) && isMoving;
 
-        Vector3 velocity = (transform. forward * currentDir.y + transform.right * currentDir.x) * Speed + Vector3.up * velocityY;
-        controller.Move(velocity * Time.deltaTime);
+    // Apply movement
+    float speed = isRunning ? walkSpeed * sprintMultiplier : walkSpeed;
+    Vector3 move = (transform.forward * moveZ + transform.right * moveX) * speed;
+    controller.Move(move * Time.deltaTime); // Move using CharacterController
 
-        if (isGrounded && Input.GetButtonDown("Jump"))
-        {
-
-            velocityY = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-        if(isGrounded! && controller.velocity.y < -1f)
-            {
-            velocityY = -8f;
-            }
+    if(move == Vector3.zero){
+        anim.SetFloat("speed",0);
     }
+    else if(!Input.GetKey(KeyCode.LeftShift)){
+        anim.SetFloat("speed",0.5f);
+    }
+    else{
+        anim.SetFloat("speed",1f);
+    }
+
+    // Apply gravity
+    velocityY += gravity * Time.deltaTime;
+    controller.Move(Vector3.up * velocityY * Time.deltaTime);
+}
+
 }
